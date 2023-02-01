22d9284edc339b8cf7bc4a7f7583406cbd9faa67
心月 <727370630@qq.com>
2023-02-01 17:40:36 +0800

    二叉树编写中，临时保存

---
+++ SourceFile/XAlgorithm/XBinaryTree/XBinaryTreeObject.c
@@ -0,0 +1,70 @@
+﻿#include "XBinaryTreeObject.h"
+#include"XContainerObject.h"
+#include"XStack.h"
+#include<stdlib.h>
+#include<string.h>
+TreeNode* TreeNode_creation(const size_t TypeSize)
+{
+	struct TreeNode* node =(struct TreeNode*)malloc(sizeof(struct TreeNode));
+	if (isObjectNULL(node,"CreationTreeNode-node"))
+		return NULL;
+	node->data= calloc(1,TypeSize);//开辟内存并且置为0
+	if (isObjectNULL(node->data, "CreationTreeNode-node->data"))
+	{
+		free(node);
+		return NULL;
+	}
+	node->LeftChild = NULL;
+	node->parent = NULL;
+	node->RightChild = NULL;
+	return node;
+}
+
+const bool TreeNode_insertData(struct TreeNode* this_node, const void* LPData, const size_t TypeSize)
+{
+	if (isObjectNULL(this_node, "InsertTreeNodeData-this_node"))
+		return false;
+	if (isObjectNULL(LPData, "InsertTreeNodeData-LPData"))
+		return false;
+	if (isObjectNULL(TypeSize, "InsertTreeNodeData-TypeSize"))
+		return false;
+	memcpy(this_node->data, LPData, TypeSize);
+	return true;
+}
+
+const bool TreeNode_free(struct TreeNode* this_node , const bool parentSetNull)
+{
+	if (isObjectNULL(this_node, "TreeNode_free-this_node"))
+		return false;
+	//释放数据
+	free(this_node->data);
+	if(parentSetNull)
+	//在父节点将指向此节点的指针置空NULL
+	struct TreeNode* LPparent = this_node->parent;
+	if (LPparent->LeftChild == this_node)
+		LPparent->LeftChild = NULL;
+	else if (LPparent->RightChild == this_node)
+		LPparent->RightChild = NULL;
+	//释放节点
+	free(this_node);
+	return true;
+}
+
+const size_t Tree_freeAll(struct TreeNode* this_root)
+{
+	if (isObjectNULL(this_root, "Tree_freeAll-this_root"))
+		return 0;
+	size_t sum = 0;//一共释放了几个节点
+	XStack* stack = XStack_init("struct TreeNode*",sizeof(struct TreeNode*));
+	XStack_Push(stack,&this_root);
+	struct TreeNode* currentNode = NULL;//当前节点指针
+	while (!XStack_empty(stack))
+	{
+		currentNode = *(struct TreeNode**)XStack_top(stack);
+		if(currentNode->LeftChild!=NULL)
+			XStack_Push(stack, &currentNode->LeftChild);
+		if (currentNode->RightChild != NULL)
+			XStack_Push(stack, &currentNode->RightChild);
+	}
+	return sum;
+}
---
+++ SourceFile/XAlgorithm/XBinaryTree/XBinaryTreeObject.h
@@ -0,0 +1,22 @@
+﻿//二叉树基类
+#ifndef XBINARYTREEOBJECT_H
+#define XBINARYTREEOBJECT_H
+#include<stdio.h>
+#include<stdbool.h>
+//二叉树节点
+typedef struct TreeNode
+{
+	struct TreeNode* parent;//父节点
+	struct TreeNode* LeftChild;//左孩子节点
+	struct TreeNode* RightChild;//右孩子节点
+	void* data;//数据指针
+}TreeNode;
+//创建初始化一个二叉树节点
+struct TreeNode* TreeNode_creation(const size_t TypeSize);
+//插入数据
+const bool TreeNode_insertData(struct TreeNode* this_node,const void* LPData, const size_t TypeSize);
+//释放一个树节点,parentSetNull父节点指向的指针置为空
+const bool TreeNode_free(struct TreeNode* this_node,const bool parentSetNull);
+//释放整个树
+const size_t Tree_freeAll(struct TreeNode* this_root);
+#endif // !XBINARYTREEOBJECT_H
