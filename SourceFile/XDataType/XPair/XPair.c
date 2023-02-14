b0db86804713922486666ce5142685d985ef553f
心月 <727370630@qq.com>
2023-02-15 00:10:07 +0800

    添加pair

---
+++ SourceFile/XDataType/XPair/XPair.c
@@ -0,0 +1,22 @@
+﻿#include"XPair.h"
+#include"XContainerObject.h"
+#include<stdlib.h>
+#include<string.h>
+XPair* XPair_init(const size_t firstTypeSize, const size_t secondTypeSize)
+{
+	if (firstTypeSize == 0 || secondTypeSize == 0)
+	{
+		printf("有类型设置错误");
+		return NULL;
+	}
+	XPair* this_pair = (XPair*)malloc(sizeof(XPair));
+	if (isNULL(isNULLInfo(this_pair, "初始化pair结构体失败")))
+		return NULL;
+	this_pair->first = calloc(1,firstTypeSize);//开辟的同时初始化为零
+	if (isNULL(isNULLInfo(this_pair->first, "初始化pair-first失败")))
+		return NULL;
+	this_pair->second = calloc(1,secondTypeSize);
+	if (isNULL(isNULLInfo(this_pair->second, "初始化pair-second失败")))
+		return NULL;
+	return this_pair;
+}
---
+++ SourceFile/XDataType/XPair/XPair.h
@@ -0,0 +1,12 @@
+﻿#ifndef XPAIR_H
+#define XPAIR_H
+#include<stdio.h>
+typedef struct XPair//pair是将2个数据组合成一组数据，当需要这样的需求时就可以使用pair
+{
+	void* first;//第一组数据
+	void* second//第二组数据
+}XPair;
+//开辟一个XPair,初始化
+#define XPair_Init(firstType,secondType) XPair_init(sizeof(firstType),sizeof(secondType))
+XPair* XPair_init(const size_t firstTypeSize, const size_t secondTypeSize);
+#endif
