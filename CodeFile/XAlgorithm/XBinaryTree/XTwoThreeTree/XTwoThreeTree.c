a0f63722c610e08c744444bd4779b9b0f4743f5c
心月 <727370630@qq.com>
2023-02-26 22:59:38 +0800

    23树算法建立文件夹

---
+++ CodeFile/XAlgorithm/XBinaryTree/XTwoThreeTree/XTwoThreeTree.c
@@ -0,0 +1,1 @@
+﻿#include"XTwoThreeTree.h"
---
+++ CodeFile/XAlgorithm/XBinaryTree/XTwoThreeTree/XTwoThreeTree.h
@@ -0,0 +1,13 @@
+﻿//23树
+#ifndef XTWOTHREETREE_H
+#define XTWOTHREETREE_H
+#include "XBinaryTreeObject.h"
+#include"XFunctionCallback.h"
+//23树节点
+typedef struct XTTTreeNode
+{
+	XBTreeNode object;
+
+}XTTTreeNode;
+
+#endif // !XTWOTHREETREE_H
