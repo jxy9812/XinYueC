866fdc0574525320872638f1089a88ee776c4a4f
心月 <727370630@qq.com>
2024-03-04 00:57:19 +0800

    修改cmake配置文件支持安装成动静态库

---
+++ CMake_Config_template.cmake
@@ -0,0 +1,29 @@
+﻿if(MSVC)
+#在MSVC上必须指定 /Zc:__cplusplus 选项    
+    add_compile_options(/Zc:__cplusplus)
+endif()
+if(WIN32)
+    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)#windows系统动态库生成lib文件命令
+    set(CMAKE_PREFIX_PATH "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/install/${CMAKE_BUILD_TYPE}") # Qt Kit Dir
+    set(CMAKE_INSTALL_PREFIX "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/install/${CMAKE_BUILD_TYPE}") # 安装目录
+elseif(UNIX)
+    set(CMAKE_PREFIX_PATH "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/install/${CMAKE_BUILD_TYPE}") # Qt Kit Dir
+    set(CMAKE_INSTALL_PREFIX "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/install/${CMAKE_BUILD_TYPE}") # 安装目录
+endif()
+#显示
+message(STATUS "CMAKE_BUILD_TYPE= ${CMAKE_BUILD_TYPE}")	
+message(STATUS "CMAKE_PREFIX_PATH= ${CMAKE_PREFIX_PATH}")	
+message(STATUS "CMAKE_INSTALL_PREFIX= ${CMAKE_INSTALL_PREFIX}")	
+#自动查找头文件路径函数(没有去重)
+macro(FIND_INCLUDE_DIR result curdir)  #定义函数,2个参数:存放结果result；指定路径curdir；
+    file(GLOB_RECURSE children "${curdir}/*.hpp" "${curdir}/*.h" )	#遍历获取{curdir}中*.hpp和*.h文件列表
+    message(STATUS "children= ${children}")								#打印*.hpp和*.h的文件列表
+    set(dirlist "")														#定义dirlist中间变量，并初始化
+    foreach(child ${children})											#for循环
+        string(REGEX REPLACE "(.*)/.*" "\\1" LIB_NAME ${child})			#字符串替换,用/前的字符替换/*h
+        if(IS_DIRECTORY ${LIB_NAME})									#判断是否为路径
+            LIST(APPEND dirlist ${LIB_NAME})							#将合法的路径加入dirlist变量中
+        endif()															#结束判断
+    endforeach()														#结束for循环
+    set(${result} ${dirlist})											#dirlist结果放入result变量中
+endmacro()																#函数结束
---
+++ CMake_Install.cmake
@@ -0,0 +1,50 @@
+﻿ #安装头文件
+install(FILES ${PUBLIC_HDRS} DESTINATION "include/${EXPORT_NAME}")
+
+set_target_properties(${EXPORT_NAME} PROPERTIES
+    OUTPUT_NAME ${EXPORT_NAME}
+    VERSION ${PROJECT_VERSION}
+    SOVERSION ${PROJECT_VERSION}
+    PUBLIC_HEADER "${PUBLIC_HDRS}"
+)
+
+#安装库
+ install(TARGETS ${EXPORT_NAME} ${EXPORT_NAME}static
+        EXPORT ${EXPORT_NAME}Targets # 导出
+        RUNTIME DESTINATION bin
+        ARCHIVE DESTINATION lib
+        LIBRARY DESTINATION lib
+ )
+
+# 生成 xxxTargets.cmake文件
+install(
+	EXPORT ${EXPORT_NAME}Targets
+	DESTINATION lib/cmake/${EXPORT_NAME}
+    FILE ${EXPORT_NAME}Targets.cmake
+    #NAMESPACE ${EXPORT_NAME}::
+)
+#======================生成 xxxConfig.cmake===============================
+# 该变量会通过xxxConfig.cmake.in用于在生成的xxxConfig.cmake中
+set(INCLUDE_DIRS include/${EXPORT_NAME})
+set(LIBRARIES ${EXPORT_NAME})
+set(LIB_DIR lib)
+
+# 由cmake提供
+include(CMakePackageConfigHelpers)
+
+# 生成 xxxConfigVersion.cmake文件
+write_basic_package_version_file(
+	${PROJECT_BINARY_DIR}/${EXPORT_NAME}ConfigVersion.cmake
+	VERSION ${VERSION}
+	COMPATIBILITY SameMajorVersion
+)
+# 用于生成 xxxConfig.cmake文件
+configure_package_config_file(${CMAKE_CURRENT_SOURCE_DIR}/${EXPORT_NAME}Config.cmake.in
+                              "${CMAKE_CURRENT_BINARY_DIR}/${EXPORT_NAME}Config.cmake"
+                              INSTALL_DESTINATION lib/cmake/${EXPORT_NAME}
+                              PATH_VARS INCLUDE_DIRS LIBRARIES LIB_DIR
+	                          INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX})
+install(
+	FILES ${PROJECT_BINARY_DIR}/${EXPORT_NAME}Config.cmake ${PROJECT_BINARY_DIR}/${EXPORT_NAME}ConfigVersion.cmake
+	DESTINATION lib/cmake/${EXPORT_NAME}
+)
