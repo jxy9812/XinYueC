0f15ed1d35aacce4a35213c3e089487f7eee09e9
xin-yue <727370630@qq.com>
2025-05-21 21:15:43 +0800

    XMutex 创建

---
+++ Src/XCustomStruct/XMutex/XMutex.h
@@ -0,0 +1,34 @@
+﻿#ifndef XMUTEX_H
+#define XMUTEX_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include<stdbool.h>
+#include<stdint.h>
+typedef struct XMutex XMutex;
+//自定义队列接口
+typedef struct XMutex_Port
+{
+	bool (*create_funcPointer)(XMutex* mutex);
+	void (*free_funcPointer)(XMutex* mutex);
+	bool (*lock_funcPointer)(XMutex* mutex);
+	bool (*unlock_funcPointer)(XMutex* mutex);
+	bool (*lockISR_funcPointer)(XMutex* mutex);
+	bool (*unlockISR_funcPointer)(XMutex* mutex);
+}XMutex_Port;
+//互斥锁
+typedef struct XMutex
+{
+	void* m_mutex;//
+	XMutex_Port m_port;
+}XMutex;
+XMutex* XMutex_new(XMutex_Port* port);
+void XMutex_free(XMutex* mutex);
+bool XMutex_lock(XMutex* mutex);
+bool XMutex_unlock(XMutex* mutex);
+bool XMutex_lockISR(XMutex* mutex);
+bool XMutex_unlockISR(XMutex* mutex);
+#ifdef __cplusplus
+}
+#endif
+#endif // !XEventQueue_H
