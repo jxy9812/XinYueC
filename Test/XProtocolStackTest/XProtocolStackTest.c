88e60b954b7a72b22d3060f88836cb86e4840d35
xinyue <727370630@qq.com>
2026-08-04 20:44:25 +0800

    移除旧协议栈模块及相关测试

--- Test/XProtocolStackTest/XProtocolStackTest.c
+++ Test/XProtocolStackTest/XProtocolStackTest.c
@@ -5,8 +5,6 @@ void XMenu_XProtocolStackTest(XMenu* root)
 {
 	XMenu* menu = XMenu_create("协议栈");
 	XMenu_addMenu(root, menu);
-	XMenu_XDataFrameCommTest(menu);
-	XMenu_TJCHMICommTest(menu);
 	XMenu_XModbusTest(menu);
 	XMenu_XMqttTest(menu);
 	XMenu_XCanTest(menu);
